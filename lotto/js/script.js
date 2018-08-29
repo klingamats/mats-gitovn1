00b1a3dcdab91911e28699527dd8dd82780328ae
klingamats <mjklinga@gmail.com>
2018-08-29 10:53:57 +0300

    script.js gjord och länkad till index.html

--- lotto/js/script.js
+++ lotto/js/script.js
@@ -1,6 +1,7 @@
 //slumptal mellan 1 och 39
 //OBS dålig kod, mjlighet till dublikat => använd inte ;)
 function lottoRad() {
+	alert("lottoraden klar!");
 	for (var i=0; i<=7; i++) {
 
 		let lottery = Math.floor(Math.random() * 38)+1;
